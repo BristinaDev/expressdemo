eee204ac1f6ac6a1258576129960234d4d560f8c
BristinaDev <devmode26@gmail.com>
2025-04-25 17:07:55 +0530

    Added User routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const userController = require('../controllers/userController')
+const router = express.Router();
+
+router.get('/', userController.getAllUser)
+router.get('/:id', userController.getUserById)
+router.post("/", userController.createUser)
+router.put('/:id', userController.updateUser)
+router.delete('/:id', userController.deleteUser)
+
+export { router };
