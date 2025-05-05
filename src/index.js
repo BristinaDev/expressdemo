9ad90c438d4fb7207c5119c0d7b58c29c24bf0c4
BristinaDev <devmode26@gmail.com>
2025-05-05 11:42:45 +0530

    Basic Socket io

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import userRoutes from './routes/userRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import { graphqlHTTP } from "express-graphql";
 import User from './models/users.model.js';
+import { createServer } from 'node:http';
+import { Server } from "socket.io";
 
 import {
 	graphql,
@@ -26,10 +28,25 @@ const storage = multer.diskStorage({
 		cb(null, uniqueSuffix+file.originalname)
 	}
 })
-
 const upload = multer({storage: storage})
 const app = express()
 dotenv.config();
+
+const server = createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+	console.log("User connected:", socket.id);
+
+	socket.on('chat message', (msg) => {
+		console.log('Message received:::::', msg);
+		io.emit('chat message', msg);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User disconnected:', socket.id);
+	});
+});
 
 app.use(express.json());
 app.use('/users', userRoutes);
@@ -75,22 +92,68 @@ app.use('/graphql', graphqlHTTP({
 
 app.get('/', (req, res) => {
 	res.send(`
-			<html>
-					<body>
-							<div class="container">
-									<h1>Welcome DEMO!!</h1>
-									<p>The backend server is running!</p>
-							</div>
-					</body>
-			</html>
+	<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Socket.IO Chat</title>
+    <script src="/socket.io/socket.io.js"></script>
+    <style>
+      body { margin: 0; padding-bottom: 3rem; font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif; }
+
+      #form { background: rgba(0, 0, 0, 0.15); padding: 0.25rem; position: fixed; bottom: 0; left: 0; right: 0; display: flex; height: 3rem; box-sizing: border-box; backdrop-filter: blur(10px); }
+      #input { border: none; padding: 0 1rem; flex-grow: 1; border-radius: 2rem; margin: 0.25rem; }
+      #input:focus { outline: none; }
+      #form > button { background: #333; border: none; padding: 0 1rem; margin: 0.25rem; border-radius: 3px; outline: none; color: #fff; }
+
+      #messages { list-style-type: none; margin: 0; padding: 0; }
+      #messages > li { padding: 0.5rem 1rem; }
+      #messages > li:nth-child(odd) { background: #efefef; }
+    </style>
+  </head>
+  <body>
+    <ul id="messages"></ul>
+    <form id="form" action="">
+      <input id="input" autocomplete="off" /><button>Send</button>
+    </form>
+
+    <script>
+      document.addEventListener('DOMContentLoaded', () => {
+        const socket = io();
+
+        const form = document.getElementById('form');
+        const input = document.getElementById('input');
+        const messages = document.getElementById('messages');
+
+        form.addEventListener('submit', (e) => {
+          e.preventDefault();
+          if (input.value) {
+            console.log('Sending message:->>>>', input.value);
+            socket.emit('chat message', input.value);
+            input.value = '';
+          }
+        });
+
+        socket.on('chat message', (msg) => {
+          console.log('Received message::::::::::::::::', msg);
+          const item = document.createElement('li');
+          item.textContent = msg;
+          messages.appendChild(item);
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+      });
+    </script>
+  </body>
+</html>
 	`);
 });
+
 
 console.log(listEndpoints(app));
 
 connectionDB();
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,20 +5,8 @@ const userSchema = new Schema (
     username: { type: String, required: true, unique: true,lowercase: true, index: true},
 		email: {type: String, required: true, unique: true, lowercase: true,},
 		password:  { type: String, required: true },
-		fullname: { type: String, required: true},
+		fullname: { type: String },
 		isActive: { type: Boolean, default: true },
-
-		// avatar: {
-    //   type: String, //URL
-		// 	required: true,
-		// },
-		// watchHistory: [{
-		// 	type: Schema.Types.ObjectId,
-		// 	ref: "Video"
-		// }],
-		// refreshToken: {
-		// 	type: String
-		// }
 
 	}, {timestamps: true})
   
@@ -27,18 +15,23 @@ const userSchema = new Schema (
 	// 	return this.where({ email: email, isActive: true})
 	// }
   
-	userSchema.methods.findByEmail = function (email) {
-		return this.model('User').find({ email: email });
-	};
+	// userSchema.methods.findByEmail = function (email) {
+	// 	return this.model('User').find({ email: email });
+	// };
 	
 	//Pre Middleware
-	userSchema.pre('save', function(next){
-		console.log(`[PRE] User is ${this.username}`)
-	})
+	// userSchema.pre('save', function(next){
+	// 	console.log(`[PRE] User is ${this.username}`)
+	// })
 
-	//Post Middleware
-	userSchema.post('save', function(doc){
-		console.log(`[POST] User ${doc.username} saved.`)
-	})
+	// //Post Middleware
+	// userSchema.post('save', function(doc){
+	// 	console.log(`[POST] User ${doc.username} saved.`)
+	// })
 
+	// userSchema.pre('save', async function (next){
+	// if (!this.isModified('password')) return next();
+	// this.password = await bcrypt.hash(this.password, 10);
+	// next();
+	// });
 export default mongoose.model("User", userSchema);
